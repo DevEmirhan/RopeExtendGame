a524f4042ec65f7d07f8c383e9b981f7eb587623
Emirhan Demir <66824487+DevEmirhan@users.noreply.github.com>
2020-12-22 11:24:33 +0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]
+    private float speed = 1f;
+    [SerializeField]
+    private float ropeSpeed = 1f;
+
+    public ObiRope rope;
+    public ObiRopeCursor cursor;
+    private float currentLength;
+    private float currentLengthHelper;
+    private float increaseAmount;
+    private float desiredLength;
+    private bool isReady = false;
+   
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        currentLength = rope.restLength;
+        currentLengthHelper = currentLength;
+        increaseAmount = currentLength / 10f;
+        Debug.Log("Rope length is" + currentLength);
+        Debug.Log("Increase length is" + increaseAmount);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float mH = Input.GetAxis("Horizontal");
+        float mV = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(mH * speed, rb.velocity.y, mV * speed);
+    }
+    public void ExtendRope(int power)
+    {
+        desiredLength = currentLength + (increaseAmount * power);
+        //cursor.ChangeLength(desiredLength);
+        currentLength = desiredLength;
+        isReady = true;
+    }
+
+    private void Update()
+    {
+        if (isReady)
+        {
+            currentLengthHelper = Mathf.SmoothStep(currentLengthHelper, desiredLength, Time.deltaTime*ropeSpeed);
+            
+            cursor.ChangeLength(currentLengthHelper);
+        }
+    }
+
+}
+
---
+++ Assets/RopeExtender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeExtender : MonoBehaviour
+{
+    private bool isActive = true;
+    public int IncreasePower = 1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && isActive)
+        {
+            other.gameObject.GetComponent<PlayerController>().ExtendRope(IncreasePower);
+            isActive = false;
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
+
